daee613462abf45df0afa0bc746c1a9f59d187cd
Marco Antonio Gutierrez <52805694+KyM1809@users.noreply.github.com>
2021-01-04 11:39:33 -0600

    Update Conexion.php

--- php/Conexion.php
+++ php/Conexion.php
@@ -4,6 +4,6 @@
 		$pdo = new PDO($ConnectionString, USUARIO, PASSWORD, array(PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES utf8"));
 		echo "<script>console.log('Conectado')</script>";
 	}catch(PDOException $Ex){
-		echo "<script>console.log('Error')</script>";
+		echo "<script>console.log('Error de conexion')</script>";
 	}
 ?>
